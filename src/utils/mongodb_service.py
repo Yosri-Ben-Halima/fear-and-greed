772588cbcc2fce11fd4e7f27a975b522237afc90
Yosri Ben Halima <yosri@laevitas.ch>
2024-10-16 15:53:36 +0100

    Push

--- src/utils/mongodb_service.py
+++ src/utils/mongodb_service.py
@@ -1,6 +1,14 @@
 from pymongo import MongoClient
 from typing import Literal, Optional
 from datetime import datetime
+import yaml
+
+with open("config/secrets.yml", "r") as file:
+    secrets = yaml.safe_load(file)
+
+mongo_credential = (
+    secrets.get("mongodb_credential", {}).get("crypto_data", {}).get("host")
+)
 
 
 def parse_futures(input_data: dict) -> dict:
@@ -46,6 +54,7 @@ def parse_options(input_data: dict) -> dict:
 
     return output_data
 
+
 def parse_pc(input_data: dict) -> dict:
     # Extract values from the input data
     points = input_data.get("points", {})
@@ -60,6 +69,7 @@ def parse_pc(input_data: dict) -> dict:
 
     return output_data
 
+
 def parse_perps(input_data: dict) -> dict:
     # Extract values from the input data
     points = input_data.get("points", {})
@@ -70,14 +80,21 @@ def parse_perps(input_data: dict) -> dict:
         "date": input_data["date"],
         "currency": input_data["currency"],
         "price": points.get("0", {}).get("price"),  # Extracting price
-        "open_interest": points.get("0", {}).get("open_interest"),  # Extracting open interest
+        "open_interest": points.get("0", {}).get(
+            "open_interest"
+        ),  # Extracting open interest
         "volume": points.get("0", {}).get("volume"),  # Extracting volume
         "funding": points.get("0", {}).get("funding"),  # Extracting basis
-        "liquidations_long": points.get("0", {}).get("liquidations_long"), # Extracting liquidations long side
-        "liquidations_short": points.get("0", {}).get("liquidations_short"), # Extracting liquidations short side
+        "liquidations_long": points.get("0", {}).get(
+            "liquidations_long"
+        ),  # Extracting liquidations long side
+        "liquidations_short": points.get("0", {}).get(
+            "liquidations_short"
+        ),  # Extracting liquidations short side
     }
-    
+
     return output_data
+
 
 def get_data(
     coin: Literal["BTC", "ETH"],
@@ -86,9 +103,23 @@ def get_data(
     end: Optional[str] = None,
     limit: Optional[int] = None,
 ) -> list:
-    client = MongoClient("mongodb://localhost:27017/")
-    db = client["laevitas"]
-    collection = db[type]
+    client = MongoClient(mongo_credential)
+
+    db_mapper = {
+        "futures": "laevitas_futures",
+        "options": "laevitas_futures",
+        "perpetuals": "laevitas_futures",
+        "pc_ratio": "laevitas_test",
+    }
+    collection_mapper = {
+        "futures": "alt_future_summary",
+        "options": "alt_option_summary",
+        "perpetuals": "alt_summary",
+        "pc_ratio": "ratio_pc",
+    }
+
+    db = client[db_mapper[type]]
+    collection = db[collection_mapper[type]]
 
     # Create the base query for currency and hour
     query = {
@@ -111,12 +142,12 @@ def get_data(
 
     if type == "futures":
         return [parse_futures(res) for res in list(results)]
-    
+
     if type == "options":
         return [parse_options(res) for res in list(results)]
-    
+
     if type == "pc_ratio":
         return [parse_pc(res) for res in list(results)]
-    
+
     if type == "perpetuals":
         return [parse_perps(res) for res in list(results)]
